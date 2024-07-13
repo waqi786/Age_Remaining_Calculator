b69943c92623ea69f6f04ba24a65de1cc83c4718
Waqar Ali <w.ali2020838@gmail.com>
2024-07-13 04:18:10 -0700

    First Commit

---
+++ Age_Remaining_Calculator.py
@@ -0,0 +1,7 @@
+print("Total age is 90 years")
+a = int(input("Enter your current age: "))
+b = 90-a
+c = b*365
+d = c // 30
+e = b*365//7
+print(f"Year_left: {b}, Days_left: {c}, Month_left: {d}, Week_left: {e}")
